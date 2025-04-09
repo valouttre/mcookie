c43b254a0ed5b003f85aad7dc9ac283c3cd0a74f
valouttre <deroxi.ytb@gmail.com>
2025-04-09 03:37:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ function App() {
   const buyUpgrade = (upgrade) => {
     const cost = Math.floor(upgrade.baseCost * Math.pow(1.15, upgrade.count));
     
-   const cost = upgrade.cost; {
+   const cost = .cost; {
       setCookies(prev => prev - cost);
       setUpgrades(prev => prev.map(u => {
         if (u.id === upgrade.id) {
